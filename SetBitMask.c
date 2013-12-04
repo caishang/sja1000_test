42501d61102883e21f2b34d348ba5b0ded6c283c
shrygz <shrygz@gmail.com>
2013-12-04 14:16:25 +0800

    6

--- SetBitMask.c
+++ SetBitMask.c
@@ -1,5 +1,23 @@
 /×
-
-
+	函数名称：SetBitMask
+	函数功能：设置指定寄存器的特定位为1
+	输入参数：RegAdr:寄存器地址 BitValue:设置的位值
+	输出参数：无
+	返回值：1=成功0=失败
 ×/
-char SetBitMask()
+char SetBitMask(unsigned char RegAdr,unsigned char BitValue)
+{
+	char status = 0;
+	unsigned char temp;
+	temp = ReadSJAReg(RegAdr);
+	temp = temp|BitValue;	//置1为或运算
+	WriteSJAReg(RegAdr,temp);
+	
+	//判断置位成功与否
+	if(ReadSJAReg(RegAdr)==temp)
+	{
+		status=1;
+	}
+	else status=0;
+	return (status);
+}
